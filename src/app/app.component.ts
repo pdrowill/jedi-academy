6953f82643e2e13c8fb343973291663ca5706174
Pedro Wilson <pdro.wilson@gmail.com.brgit config --global user.emailgit config --global user.email pdro.wilson@gmail.com>
2019-07-30 23:51:16 -0300

    Component, Property Bindings e decorator Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ja-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  luke = {name: "Luke", isJedi: true, temple: "Coruscant"}  
+  leia = {name: "Leia", isJedi: false}  
+  han = {name: "Han Solo", isJedi: false}  
 }
